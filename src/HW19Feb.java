1a842bec60dda52a3f1c58ec7cb95aeaf501172a
Udhays <linusai06@gmail.com>
2020-02-19 13:31:46 +0530

    Home Work Practice

---
+++ src/HW19Feb.java
@@ -0,0 +1,38 @@
+
+public class HW19Feb {
+
+	public static void main(String[] args) {
+		int n1=10,n2=20;
+		int c,d;
+		c= n1+n2;
+	// This Syntax is used to declare the String
+		String Sdec = "Java";
+		String Sdec1 ="Test";
+	// This Syntax is a Compound Assignment sample 	
+		d= +n2;
+	// This Syntax is a Arth operator O/P  
+		System.out.println(c);
+	// This Syntax is a Compound assignment O/P
+		 System.out.println(d);
+	 // The below statement is a Equality Operator "  
+		System.out.println(c==d);
+	// The below statement is a Equality operator "Not Equality
+	     System.out.println(c!= d);
+	// The below syntax is a String 
+	     System.out.println (Sdec+Sdec1+n1+n2) ;
+	// The below syntax is a String Concatention  
+	     System.out.println (Sdec+Sdec1+(n1+n2)) ;
+	// The below syntax is a Addition of Strings 
+	     System.out.println (n1+n2+Sdec+Sdec1) ;
+	// The below syntax is for boolean true
+	     System.out.println (n1<n2);
+	// The below syntax is for boolean false
+	     System.out.println (n1>n2);
+	// The below syntax is for boolean true
+	     System.out.println (n1<= n2);
+	// The below syntax is for boolean true
+	     System.out.println (n1>= n2);
+	}
+
+}
+
