b00a7cdbfecebc3e2668829577fc5b78e6aaa590
Udhays <u.linusai@gmail.com>
2020-02-20 10:06:45 +0530

    Feb 20 Class work

---
+++ src/Conditions.java
@@ -0,0 +1,16 @@
+
+public class Conditions {
+
+	public static void main(String[] args) {
+	
+		if ('b' < 'a' )
+		{
+			System.out.println( "The value should be hi ");
+		}
+		else
+		{
+			System.out.println("The value should be Hello");
+		}
+	}
+
+}
