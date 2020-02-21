9b556945e5a92b7b9dfc2d42c755223cf6e54987
Udhays <u.linusai@gmail.com>
2020-02-21 09:02:46 +0530

    Feb 21 Class work on the Switch

---
+++ src/HW21Feb.java
@@ -0,0 +1,30 @@
+
+public class HW21Feb {
+
+	public static void main(String[] args) {
+		int n=1;
+		switch (n){
+		case 1 :
+		System.out.println("Print the 1st Case");
+		break ;
+		case 2 :
+		System.out.println("Print the 2nd Case");
+		break ;
+		case 3 :
+		System.out.println("Print the 3rd Case");
+		break ;
+		case 4 :
+		System.out.println("Print the 4th Case");
+		break ;
+		case 5 :
+		System.out.println("Print the 5th Case");
+		break ;
+		default :
+		System.out.println("Invalid Selection");
+		}
+		
+		
+
+	}
+
+}
