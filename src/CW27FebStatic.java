60359102a63c2e9c0d5f73e1a623375b76753773
udhays <u.linusai@gmail.com>
2020-02-27 10:13:20 +0530

    CW 27feb

---
+++ src/CW27FebStatic.java
@@ -0,0 +1,17 @@
+
+public class CW27FebStatic {
+	static int x=10;
+	static int y=20;
+public void add( ) {
+	System.out.println("The Non Static variable assignment" + x+y);
+	}
+public static void sub() {
+	int X=200; int Y= 300;
+	System.out.println("The Static variable assignment" + (Y-X));
+}
+public static void main(String args[]) { 
+	
+	sub();
+}
+}
+
