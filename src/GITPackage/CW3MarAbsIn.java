bafdcdcb5ecb38065c9fd0b8449225c553777ca9
udhays <u.linusai@gmail.com>
2020-03-03 09:51:16 +0530

    CW3MAr Abstration Class

---
+++ src/GITPackage/CW3MarAbsIn.java
@@ -0,0 +1,20 @@
+package GITPackage;
+
+public class CW3MarAbsIn extends CW3MarAbstraction{
+	
+	@Override
+	public void abmethod() {
+		// TODO Auto-generated method stub
+		//System.out.println("Enter the Miles:");
+		System.out.println("Print the Child Class Conversion gal to lts :" +galtolts );
+	}
+	public static void main (String[] args) { 
+		CW3MarAbsIn n= new CW3MarAbsIn ();
+		n.noabsmethod();
+		n.abmethod();
+		
+	
+		
+		
+	}
+}
---
+++ src/GITPackage/CW3MarAbstraction.java
@@ -0,0 +1,25 @@
+package GITPackage;
+
+import java.util.Scanner;
+
+//Act of reprsentating the data without showing the internal implementation (1 Abstract Class & Interface)
+public abstract class CW3MarAbstraction {
+	public void noabsmethod() {
+		System.out.println("Enter the Miles:");
+		Scanner dataentry = new Scanner (System.in); // Syntax to use and call the user input
+		double miles =dataentry.nextDouble();
+		double kms = 1.60934;
+		double milestokms = miles * kms;
+		System.out.println("Print the Child Class Conversion miles to kms :" +milestokms );
+		
+	}
+	public abstract void abmethod (); //Abstract method is called in another class 
+
+	//Scanner dataentry = new Scanner (System.in); // Syntax to use and call the user input
+	double gal =10;
+	double lts = 3.784;
+	double galtolts = gal * lts;
+	
+}
+
+
